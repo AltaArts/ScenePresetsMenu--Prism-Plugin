8f6e680596fc6c19bd70b9087e974c4c42c2ff39
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-04-06 08:36:51 -0400

    Updated for Prism 2.0.5

--- ScenePresetsMenu/Scripts/Prism_ScenePresetsMenu_Functions.py
+++ ScenePresetsMenu/Scripts/Prism_ScenePresetsMenu_Functions.py
@@ -44,15 +44,12 @@
 ####################################################
 
 
-try:
-    from PySide2.QtCore import *
-    from PySide2.QtGui import *
-    from PySide2.QtWidgets import *
-except:
-    from PySide.QtCore import *
-    from PySide.QtGui import *
+import os
+import json
 
-import os, json
+from qtpy.QtCore import *
+from qtpy.QtGui import *
+from qtpy.QtWidgets import *
 
 from PrismUtils.Decorators import err_catcher_plugin as err_catcher
 
